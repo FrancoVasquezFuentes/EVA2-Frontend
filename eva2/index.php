7792ac3c9487e282d9fab3fb3041e2fcb04ba2bf
FrancoVasquezFuentes <167715608+FrancoVasquezFuentes@users.noreply.github.com>
2024-05-18 13:16:15 -0400

    Add files via upload

---
+++ eva2/index.php
@@ -0,0 +1,367 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ENERGY AND WATER SPA</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="estilos.css">
+</head>
+
+<body>
+  <nav class="navbar sticky-top navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">E&W SPA</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-link">
+            <a class="nav-link active" aria-current="page" href="#Historia">Historia</a>
+          </li>
+          <li class="nav-link">
+            <a class="nav-link" href="#Equipo">Equipo</a>
+          </li>
+          <li class="nav-link">
+            <a class="nav-link" href="#Preguntas Frecuentes">Preguntas Frecuentes</a>
+          </li>
+          <li class="nav-link">
+            <a class="nav-link" href="#Contactenos">Contactenos</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <img src="./imagenes/4c06afb30b36d6b15132b0123f35d4da_fit.com-JsW9gQt_f0DyAq5xCJV6zthJnDrFB9xNT_7Zi0dheuPPGv82nVuYHe2Y84XRJFggmmzcKHuvqb_wD3-RbhWgWSg_w16383.JPG" alt="Imagen" class="img-fluid">
+      </div>
+      <div class="col-12 texto-superpuesto">
+        <div class="col-md">
+          <h2 class="text-center">Mantenimiento para edificios</h2>
+          <p class="text-center">CLIMATIZACIÓN, ELECTRICIDAD Y OBRAS MENORES DE CONSTRUCCIÓN.</p>
+        </div>
+        <div class="container p">
+          <div class="p-3">
+            <h2>Mantenimiento</h2>
+            <p>Conjunto de operaciones y cuidados necesarios para que tus instalaciones continúen funcionando correctamente.</p>
+            <h2>Mantenimiento preventivo</h2>
+            <p>Ayuda a prolongar la vida útil de tus activos y aumenta la productividad, a través de una revisión.</p>
+            <h2>Mantenimiento correctivo</h2>
+            <p>Corrige problemas o daños en las instalaciones o equipos.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-5">
+        <div class="card mb-3" style="max-width: 540px;">
+          <div class="row g-0">
+            <div class="col-md-3">
+              <img src="./imagenes/3cf1905198e9763be7cf8a3276a8375b_559x1125_79x0_771x1125_crop.jpg" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-9">
+              <div class="card-body">
+                <h5 class="card-title">Aire acondicionado</h5>
+                <p class="card-text">Instalamos, reparamos y revisamos todo tipo de sistemas de aire acondicionado, tanto domésticos como industriales. Nos encargamos de la limpieza, el cambio de filtros, la recarga de gas y la detección y solución de averías. </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card mb-3" style="max-width: 540px;">
+          <div class="row g-0">
+            <div class="col-md-3">
+              <img src="./imagenes/1dff1ecb88d5aa605aa877c96cd9df8b_640x1116_fit.jpg" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-9">
+              <div class="card-body">
+                <h5 class="card-title">Grupo electrógeno</h5>
+                <p class="card-text">Instalamos, reparamos y revisamos todo tipo de grupos electrógenos, tanto monofásicos como trifásicos. Nos encargamos del suministro de combustible, el cambio de aceite, el control de baterías y la verificación del funcionamiento correcto </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-5">
+        <div class="card mb-3" style="max-width: 540px;">
+          <div class="row g-0">
+            <div class="col-md-3">
+              <img src="./imagenes/9d0c9a5af9f0a48e29f3d3b61818457e_640x1176_fit.jpg" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-9">
+              <div class="card-body">
+                <h5 class="card-title">Sala de calderas</h5>
+                <p class="card-text">Instalamos, reparamos y revisamos todo tipo de calderas, tanto de gas como eléctricas. Nos encargamos del mantenimiento preventivo, la sustitución de piezas, la purga de radiadores y la regulación de la presión y la temperatura.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card mb-3" style="max-width: 540px;">
+          <div class="row g-0">
+            <div class="col-md-3">
+              <img src="./imagenes/6f174bfc2c2d263b250ab7bee3d6255f_640x1116_fit.jpg" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-9">
+              <div class="card-body">
+                <h5 class="card-title">Sala de bombas</h5>
+                <p class="card-text">Instalamos, reparamos y revisamos todo tipo de bombas hidráulicas, tanto centrífugas como periféricas. Nos encargamos del ajuste de válvulas, el cambio de sellos mecánicos, el equilibrado de rotores y la limpieza de impulsores. </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <section id="Historia">
+    <div class="container text-center"> <!-- Nuevo contenedor para centrar -->
+      <div class="card mid-4" style="max-width: 1000px;">
+        <img src="./imagenes/historia.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Compartimos una visión clara del objetivo, fomentamos un clima de confianza, colaboración y apoyo mutuo entre los integrantes del equipo.</h5>
+          <p class="card-text">Bienvenido a Energy and Water SpA, empresa dedicada a la instalación y reparación
+            de sistemas de ventilación, calefacción, electricidad y construcción. Contamos con un equipo de
+            profesionales altamente cualificados y con amplia experiencia en el sector. Ofrecemos soluciones a medida
+            para cada cliente, garantizando la calidad y la seguridad de nuestros servicios. Contacta con nosotros y
+            solicita tu presupuesto sin compromiso.Somos una empresa fundada en 2018 con el objetivo de brindar
+            servicios integrales de mantenimiento para hogares, oficinas, comercios e industrias.
+            Nuestra misión es satisfacer las necesidades de nuestros clientes con eficiencia, rapidez y
+            profesionalidad. Nuestra visión es ser la empresa referente en el mercado por nuestra excelencia y
+            compromiso. Nuestros valores son la honestidad, la responsabilidad, el respeto y la innovación. </p>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-5">
+            <div class="card mb-3" style="max-width: 540px;">
+              <img src="./imagenes/5d11f12cdc2ed7cc9f331218cb5b9ca0_640x608_fit.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Universidad De Los Andes</h5>
+                <p class="card-text">Instalacion de paneles en "Universidad De Los Andes".</p>
+              </div>
+            </div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col-md-5">
+              <div class="card mb-3" style="max-width: 540px;">
+                <img src="./imagenes/bff51c94eebee5ca25ea6f9aeccc8491_640x608_fit.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Viña Cousiño Macul</h5>
+                  <p class="card-text">Instalaciones Luminarias en "Viña Cousiño Macul"</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+  </section>
+  <br>
+  <br>
+  <section id="Equipo">
+    <div class="container text-center"> <!-- Nuevo contenedor para centrar -->
+      <div class="card mid-4" style="max-width: 1000px;">
+        <img src="./imagenes/equipoo.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Compartimos una visión clara del objetivo, fomentamos un clima de confianza, colaboración y apoyo mutuo entre los integrantes del equipo.</h5>
+          <p class="card-text">Mizraim Abello es el fundador de Energy and Water SpA, es un líder colaborativo que se desempeña en el ámbito laboral, específicamente en el sector de la climatización y electricidad. Practica un liderazgo democrático; es decir, que involucra al equipo de trabajo en la toma de decisiones y valora sus opiniones y sugerencias. Sus principales habilidades como líder son la resolución de problemas, la comunicación efectiva, el pensamiento crítico, la capacidad de coordinación y gestión. Algunos de los logros más destacados son haber participado en la acreditación de los centros de salud de la municipalidad de San Joaquín, reparación del equipo de climatización para el museo de la Universidad de los Andes y haber realizado reparaciones y cambios de grupos electrógenos que son críticos para el funcionamiento de los edificios. Uno de los desafíos que enfrentó como líder fue adaptarse a las nuevas normativas sanitarias por la pandemia y garantizar la seguridad del equipo y los clientes. Lo logró implementando protocolos de prevención y capacitando al personal. Su objetivo como líder es darle continuidad operacional a los clientes y mejorar constantemente los procesos de mantenimiento y calidad. </p>
+        </div>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+          <div class="col">
+            <div class="card h-100">
+              <img src="./imagenes/gerentegeneral.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Gerente General</h5>
+                <p class="card-text">Descripcion Gerente General</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card h-100">
+              <img src="./imagenes/supervisor.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Supervisor</h5>
+                <p class="card-text">Descripcion Supervisor</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card h-100">
+              <img src="./imagenes/capataz.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Capataz</h5>
+                <p class="card-text">Descripcion Capataz</p>
+              </div>
+
+
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <div .accordion-body>
+      <div class="container" id="Preguntas Frecuentes">
+        <h1 class="text-center">Preguntas frecuentes</h1>
+        <div class="accordion" id="accordionExample">
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                ¿Cómo solicito una cotización?
+              </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample" aria-expanded="true" aria-controls="collapseOne">
+              <div class="accordion-body">
+                Puedes contactarnos al correo Contacto@energyandwater.cl o al +56232569798
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                ¿Necesitas visita técnica?
+              </button>
+            </h2>
+            <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample" aria-expanded="true" aria-controls="collapseOne">
+              <div class="accordion-body">
+                Puedes solicitar visita técnica para evaluación mediante correo electrónico Contacto@energyandwater.cl o al +56232569798
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                ¿Qué tipo de aire acondicionado pueden revisar?
+              </button>
+            </h2>
+            <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample" aria-expanded="true" aria-controls="collapseOne">
+              <div class="accordion-body">
+                Revisamos, reparamos e instalamos equipos domiciliarios e industriales.
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                ¿Realizan instalación de aire acondicionado domiciliario?
+              </button>
+            </h2>
+            <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample" aria-expanded="true" aria-controls="collapseOne">
+              <div class="accordion-body">
+                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container" id="Contactenos">
+          <h1 class="mt-5">Formulario de Contacto</h1>
+
+          <form id="contactForm" class="mt-3 needs-validation" action="#" method="post" novalidate>
+            <div class="row">
+              <div class="col-md-6">
+                <div class="mb-3">
+                  <label for="nombre" class="form-label">Nombre:</label>
+                  <input type="text" id="nombre" name="nombre" class="form-control" required>
+                  <div class="invalid-feedback">
+                    Por favor ingrese su nombre.
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="mb-3">
+                  <label for="apellido" class="form-label">Apellido:</label>
+                  <input type="text" id="apellido" name="apellido" class="form-control" required>
+                  <div class="invalid-feedback">
+                    Por favor ingrese su apellido.
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="mb-3">
+                  <label for="telefono" class="form-label">Teléfono:</label>
+                  <input type="tel" id="telefono" name="telefono" class="form-control" required>
+                  <div class="invalid-feedback">
+                    Por favor ingrese su teléfono.
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="mb-3">
+                  <label for="correo" class="form-label">Correo electrónico:</label>
+                  <input type="email" id="correo" name="correo" class="form-control" required>
+                  <div class="invalid-feedback">
+                    Por favor ingrese un correo electrónico válido.
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="mb-3">
+                  <label for="motivo" class="form-label">Motivo de contacto:</label>
+                  <select id="motivo" name="motivo" class="form-select" required>
+                    <option value="">Seleccionar...</option>
+                    <option value="reparacion">Reparación</option>
+                    <option value="mantencion">Mantenimiento</option>
+                    <option value="cotizacion">Cotización</option>
+                    <option value="otro">Otro</option>
+                  </select>
+                  <div class="invalid-feedback">
+                    Por favor seleccione un motivo de contacto.
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="mb-3">
+                  <label for="explicacion" class="form-label">Explicación:</label>
+                  <textarea id="explicacion" name="explicacion" class="form-control" required></textarea>
+                  <div class="invalid-feedback">
+                    Por favor ingrese una explicación.
+                  </div>
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="form-check mb-3">
+                  <input type="checkbox" id="terminos" name="terminos" class="form-check-input" required>
+                  <label for="terminos" class="form-check-label">Estoy de acuerdo con los términos y condiciones</label>
+                  <div class="invalid-feedback">
+                    Debe estar de acuerdo con los términos y condiciones.
+                  </div>
+                </div>
+              </div>
+              <div class="col-12">
+                <button type="submit" class="btn btn-primary">Enviar</button>
+              </div>
+            </div>
+          </form>
+        </div>
+
+      </div>
+
+
+
+      <script>
+        // Agregar la validación del formulario
+        document.getElementById("contactForm").addEventListener("submit", function(event) {
+          var form = event.target;
+
+          if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+
+          form.classList.add("was-validated");
+        });
+      </script>
+
+  </section>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-gXXIqexLG+QvBoW9j9WjsAWHf0a5M5R/ffgSr2mRw8rVoVHNcmiFnKqc/rXHjKG9" crossorigin="anonymous"></script>
+</body>
+
+</html>
